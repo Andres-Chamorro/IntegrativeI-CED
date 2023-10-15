08a1be6502d864f0de5793dab807a64ea996a31e
AndresBueno420 <125490078+AndresBueno420@users.noreply.github.com>
2023-10-15 17:42:05 -0500

    Revert "Feature final implentation req4"

--- src/customExceptions/NoSuchElementException.java
+++
@@ -1,8 +0,0 @@
-package customExceptions;
-
-@SuppressWarnings("serial")
-public class NoSuchElementException extends RuntimeException {
-	public NoSuchElementException(String message) {
-		super(message);
-	}
-}
